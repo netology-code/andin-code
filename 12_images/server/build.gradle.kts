47d335babbf6583841e2985e57b69c29be2014de
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2021-10-15 18:33:18 +0700

    fix(deps): tika dependency fix

--- 12_images/server/build.gradle.kts
+++ 12_images/server/build.gradle.kts
@@ -23,9 +23,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.apache.tika:tika-parsers-standard-package:2.1.0") {
-        exclude("org.apache.logging.log4j")
-    }
+    implementation("org.apache.tika:tika-core:2.1.0")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
