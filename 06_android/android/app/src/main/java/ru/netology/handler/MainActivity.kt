827746fd21e086237dbb554f9c9402a3bdd157cb
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2026-02-20 13:57:07 +0700

    fix window insets

--- 06_android/android/app/src/main/java/ru/netology/handler/MainActivity.kt
+++ 06_android/android/app/src/main/java/ru/netology/handler/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            WindowInsetsCompat.CONSUMED
         }
 
         binding.load.setOnClickListener {
